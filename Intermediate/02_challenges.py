29e5a27640a1d6581ed3aaa2ee68efd1661c11ae
Juanjo González <jjgonzalez.agudo@me.com>
2023-04-23 23:26:51 +0200

    commit

--- Intermediate/02_challenges.py
+++ Intermediate/02_challenges.py
@@ -11,6 +11,16 @@ cada impresión), sustituyendo los siguientes:
 """
 
 def fizzbuzz():
-    
+    for index in range(1, 101):
+        if index % 3 == 0 and index % 5 == 0:
+            print("fizzbuzz")
+        elif index % 3 == 0:
+            print("fizz")
+        elif index % 5 == 0:
+            print("buzz")
+        else:
+            print(index)
 
 fizzbuzz()
+
+
